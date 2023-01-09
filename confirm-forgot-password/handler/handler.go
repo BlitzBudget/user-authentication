dcfba712798a90af5ac1deb78b8d5ed5eb591181
Nagarjun <nagarjun_nagesh@outlook.com>
2023-01-09 13:42:18 +0100

    Add Readme and print as JSON

--- confirm-forgot-password/handler/handler.go
+++ confirm-forgot-password/handler/handler.go
@@ -11,12 +11,8 @@ import (
 )
 
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Printf("Processing request data for request %v.\n", request.RequestContext.RequestID)
-	fmt.Printf("Body size = %v.\n", request.Body)
-	fmt.Println("Headers:")
-	for key, value := range request.Headers {
-		fmt.Printf("    %v: %v\n", key, value)
-	}
+	jsonReq, _ := json.Marshal(request)
+	fmt.Printf("Processing request data for request %v.\n", string(jsonReq))
 
 	//service.SaveRequest(&request.Body)
 	header := map[string]string{
